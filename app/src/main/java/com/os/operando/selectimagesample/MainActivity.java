2429dc2c3d06b6ae7819276258ca8afc8bdc7ff1
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2015-01-18 17:53:32 +0900

    first commit

---
+++ app/src/main/java/com/os/operando/selectimagesample/MainActivity.java
@@ -0,0 +1,41 @@
+package com.os.operando.selectimagesample;
+
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private static final int REQUEST_GALLERY = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onSelectImage(View v) {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, REQUEST_GALLERY);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+        if (requestCode == REQUEST_GALLERY) {
+            Uri uri = data.getData();
+            if (ContentResolver.SCHEME_CONTENT.equals(uri.getScheme())) {
+                ((ImageView) findViewById(R.id.image)).setImageURI(uri);
+            }
+        }
+    }
+}
